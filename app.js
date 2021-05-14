43e4fdbf7d3d03f633fd26c19ba084b4cb0aed7d
Janine Semmler <syries@gmx.de>
2021-05-14 21:07:13 +0200

    add mustache as template engine

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 "use strict";
 
 const express = require("express");
+const mustacheExpress = require('mustache-express');
 const app = express();
 
 const ip = "127.0.0.1";
 const port = 8081;
 
-app.use(express.static("public"));
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/public');
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, ip, () => {
     console.log(`Server running at http://${ip}:${port}/`);
