b3891fb055a83104afc3f28b733cc51ec90f040c
Janine Semmler <syries@gmx.de>
2021-05-14 21:15:10 +0200

    add first openId setup with express session and passport

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const express = require("express");
+const { Issuer, Strategy } = require('openid-client');
 const mustacheExpress = require('mustache-express');
-const app = express();
+const expressSesssion = require('express-session');
+const passport = require('passport');
 
+const app = express();
 const ip = "127.0.0.1";
 const port = 8081;
 
@@ -20,3 +23,52 @@ app.get("/", (req, res) => {
 app.listen(port, ip, () => {
     console.log(`Server running at http://${ip}:${port}/`);
 });
+
+Issuer.discover('https://nodejs-sample.criipto.id')
+  .then(criiptoIssuer => {
+    var client = new criiptoIssuer.Client({
+      client_id: 'urn:criipto:nodejs:demo:1010',
+      client_secret: 'j9wYVyD3zXZPMo3LTq/xSU/sMu9/shiFKpTHKfqAutM=',
+      redirect_uris: [ `http://${ip}:${port}/login/callback` ],
+      post_logout_redirect_uris: [ `http://${ip}:${port}/logout/callback` ],
+      token_endpoint_auth_method: 'client_secret_post'
+    });
+
+    app.use(
+      expressSesssion({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true
+      })
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(
+      'oidc',
+      new Strategy({ client }, (tokenSet, userinfo, done) => {
+        return done(null, tokenSet.claims());
+      })
+    );
+    passport.serializeUser(function(user, done) {
+      done(null, user);
+    });
+    passport.deserializeUser(function(user, done) {
+      done(null, user);
+    });
+  
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+  });
